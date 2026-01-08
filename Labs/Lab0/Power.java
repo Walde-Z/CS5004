bc36ad944898aa4f460efa087ea6bbb2558fa64d
Walde-Z <498648689z@gmail.com>
2026-01-07 21:26:37 -0800

    Finish Lab0

---
+++ Labs/Lab0/Power.java
@@ -0,0 +1,14 @@
+public class Power {
+    public static void main(String[] args) {
+        long result = 1L << 31;
+        System.out.print(result);
+    }
+}
+/*
+Differences between the Java version and the Python version:
+
+1. Java requires explicit type declarations, while Python use dynamic typing and doesn't require variable types.
+2. Java programs must be complied before execution, but Python programs are interpreted and can be run directly.
+3. Java uses a class and a main method as the program entry point,
+   Python scripts execute statements directly without requiring a class.
+ */
